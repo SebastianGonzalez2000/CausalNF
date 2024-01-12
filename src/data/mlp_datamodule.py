fix sparse mlp initalizing to one sign

src/data/mlp_datamodule.py
@@ -2,10 +2,14 @@ import torch
 from torch import nn, from_numpy
 import numpy as np
 
-import os
 import torch
 from torch import utils
-import lightning.pytorch as pl
+import pytorch_lightning as pl
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 from src.data.components.dep_matrix import dependency_matrix
 
@@ -44,24 +48,25 @@ def make_sparse_mlp(sparsity_vec, n_layers, hidden_n = None):
     if hidden_n is None:
         hidden_n = d * 2
     fc1 = nn.Linear(d,hidden_n, bias = False)
-    relu1 = nn.GELU()
+    relu1 = nn.ReLU()
     fc1.weight.requires_grad = False
-    nn.init.uniform_(fc1.weight, 5.0, 10.0)
+    nn.init.normal_(fc1.weight, 0, 5)
     sparse_index = torch.where(sparsity_vec == 0)
     for idx in sparse_index:
         fc1.weight[:,idx] = torch.tensor(0.0)
     layers = [fc1, relu1]
+    print(fc1.weight.data)
     for _ in range(1, n_layers - 1):
         fc = nn.Linear(hidden_n, hidden_n, bias = False)
         fc.weight.requires_grad = False
-        nn.init.uniform_(fc.weight, 0, 1)
-        relu = nn.GELU()
+        nn.init.normal_(fc.weight, 0, 1)
+        relu = nn.ReLU()
         layers.extend([fc, relu])
     ### final layer
     fc_final = nn.Linear(hidden_n, 1, bias=False)
-    relu_final = nn.GELU()
+    relu_final = nn.ReLU()
     fc_final.weight.requires_grad = False
-    nn.init.uniform_(fc_final.weight, 0, 1)
+    nn.init.normal_(fc_final.weight, 0, 1)
     layers.extend([fc_final, relu_final])
 
     net = nn.Sequential(*layers)
@@ -89,7 +94,7 @@ class sparse_MLP(nn.Module):
 def generate_dataset_mlp(n):
 
     d = dependency_matrix.shape[0]
-    mixing_net = sparse_MLP(torch.tensor(dependency_matrix), 2)
+    mixing_net = sparse_MLP(torch.tensor(dependency_matrix), 3)
 
     # fix parameters
     for p in mixing_net.parameters():
@@ -105,14 +110,27 @@ def generate_dataset_mlp(n):
 
 
 class MLPDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size: int = 32, data_size = 30000):
+    def __init__(self, 
+                 batch_size: int = 32, 
+                 data_size = 30000,
+                 permutation = "identity" ## identity or random
+                 ):
         super().__init__()
         self.batch_size = batch_size
         self.data_size = data_size
         self.jac_batch_size = data_size // 10
+        assert permutation in ["identity", "random"], "permutation must be identity or random"
+        self.permutation = permutation
 
     def prepare_data(self):
-        self.data = generate_dataset_mlp(self.data_size)
+        self.data = generate_dataset_mlp(self.data_size) 
+        print(self.data[0])
+        if self.permutation == "random":
+            self.data = self.data[..., torch.randperm(self.data.shape[-1])]
+        print(self.data[0])
+        print(torch.mean(self.data, dim = 0))
+        print(torch.std(self.data, dim = 0))
+
 
     def setup(self, stage: str):
         self.mlp_train, self.mlp_val, self.mlp_test = utils.data.random_split(
@@ -138,3 +156,12 @@ class MLPDataModule(pl.LightningDataModule):
 
     def predict_dataloader(self):
         return utils.data.DataLoader(self.mlp_train, batch_size=self.jac_batch_size)
+
+
+if __name__ == "__main__":
+    
+    data_module = MLPDataModule(permutation="random")
+    data_module.prepare_data()
+    data_module.setup("train")
+    train_loader = data_module.train_dataloader()
+    print(next(iter(train_loader))[0:5])

src/models/sos_module.py
@@ -1,55 +1,57 @@
 import torch
 from torch import optim
+from torch.nn.functional import gaussian_nll_loss
 import lightning.pytorch as pl
 import numpy as np
+import math
 
 # define the LightningModule
 class SOSModule(pl.LightningModule):
-    def __init__(self, model, lr):
+    def __init__(self, model, lr=1e-3, weight_decay=0.0):
         super().__init__()
         self.model = model
-        self.lr = lr
+        self.lr= lr
+        self.weight_decay = weight_decay
 
     def flow_loss(self, z, logdet, size_average=True):
-        """If using Uniform as source distribution
-        log_probs = 0
+        """Assuming Standard Gaussian source distribution
         """
-        log_probs = (-0.5 * z.pow(2) - 0.5 * np.log(2 * np.pi)).sum(-1, keepdim=True)
+        log_probs = gaussian_nll_loss(z, torch.zeros_like(z), torch.ones_like(z), reduction='none')
         loss = -(log_probs + logdet).sum()
         if size_average:
-            loss /= z.size(0)
+            loss /= loss.shape[0]
+        return loss
+    
+    def training_step_(self, batch, batch_idx):
+        x = batch
+        z, logdet = self.model(x)
+
+        loss = self.flow_loss(z, logdet)
+
         return loss
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
-        x = batch
-        Z, logdet = self.model(x)
-
-        loss = self.flow_loss(Z, logdet)
+        loss = self.training_step_(batch, batch_idx)
         # Logging to TensorBoard (if installed) by default
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         # this is the validation loop
-        x = batch
-        Z, logdet = self.model(x)
-
-        val_loss = self.flow_loss(Z, logdet)
-        self.log("val_loss", val_loss)
+        loss = self.training_step_(batch, batch_idx)
+        self.log("val_loss", loss)
         self.log("Learning Rate", self.scheduler.get_last_lr()[0])
+
 
     def test_step(self, batch, batch_idx):
         # this is the test loop
-        x = batch
-        Z, logdet = self.model(x)
-
-        test_loss = self.flow_loss(Z, logdet)
-        self.log("test_loss", test_loss)
+        loss = self.training_step_(batch, batch_idx)
+        self.log("test_loss", loss)
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = optim.Adam(self.parameters(), lr=self.lr, weight_decay = self.weight_decay)
         #scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr = self.lr, total_steps = self.trainer.max_epochs, pct_start = 0.1)
         #return [optimizer], [{"scheduler": scheduler, "interval": "epoch"}]
         self.scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=self.trainer.max_epochs//100, gamma=0.97)